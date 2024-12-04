926ae199c0cd43b2dc14eceb7ac9aaa2f39c96d3
Lucas Amorim <lucas.de.amorimarq@gmail.com>
2024-12-03 23:56:08 -0300

    iniciando pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Login from './pages/login'
 
 
 
@@ -37,6 +37,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Login/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+    return (
+        <main className="App">
+            <div className="App-header">
+                <p className="App-header__title">Login</p>
+            </div>
+        </main>
+    )
+}
+export default Login;
